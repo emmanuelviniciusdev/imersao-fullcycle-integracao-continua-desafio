64da560c22c524ee7257cddfe90273df9b5fd5d8
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2024-02-12 14:56:21 -0300

    Revert "Nonsense uncovered code"
    This reverts commit 91a352a934ae165c5ba1cd2d976ae00a9668f3b8.

--- counter.js
+++ counter.js
@@ -23,32 +23,4 @@ module.exports = class Counter {
     decrement() {
         this.value--;
     }
-
-    sayHi(someInput) {
-        if (someInput === 10) {
-            throw new Error("This cannot be!");
-        } else if (someInput === "Hi") {
-            console.log("Hello!");
-        } else {
-            if (someInput) {
-                console.log("What did you say?")
-            }
-        }
-
-        console.log('Hi!');
-    }
-
-    sayHello(someInput) {
-        if (someInput === 20) {
-            throw new Error("This cannot be!");
-        } else if (someInput === "Hello") {
-            console.log("Hi!");
-        } else {
-            if (someInput) {
-                console.log("What are you talking about?")
-            }
-        }
-
-        console.log('Hello!');
-    }
 }
